4d48d356748d3d7ec4a9e7e04f74e7d9e53ba6e6
yafithekid212@gmail.com <yafithekid212@gmail.com>
2014-10-04 10:32:20 +0700

    change database user

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=gemastik',
-    'username' => 'root',
-    'password' => 'akuganteng',
+    'username' => 'vincent',
+    'password' => 'sebastian',
     'charset' => 'utf8',
 ];
